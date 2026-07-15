50489977ffd2c48c8e3f0655edcabbc954a36b34
jisulee-shsf <jisulee.shsf@gmail.com>
2026-07-16 04:51:39 +0900

    chore: 프로젝트 초기 세팅

--- service/like/src/main/java/board/like/LikeApplication.java
+++ service/like/src/main/java/board/like/LikeApplication.java
@@ -1,13 +1,12 @@
-package com.app.board;
+package board.like;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApplication {
-
+public class LikeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BoardApplication.class, args);
+        SpringApplication.run(LikeApplication.class, args);
     }
-
 }
