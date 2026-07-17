b93fa8eb94783c2134d3aec4ffbdc9276b2a7efa
jisulee-shsf <jisulee.shsf@gmail.com>
2026-07-17 20:33:48 +0900

    test: 게시물 테스트 데이터 삽입

---
+++ service/article/src/test/java/board/article/data/DataInitializer.java
@@ -0,0 +1,59 @@
+package board.article.data;
+
+import board.article.entity.Article;
+import board.common.snowflake.Snowflake;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class DataInitializer {
+
+    static final int BULK_INSERT_COUNT = 2000;
+    static final int EXECUTE_COUNT = 6000;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    TransactionTemplate transactionTemplate;
+
+    Snowflake snowflake = new Snowflake();
+    CountDownLatch countDownLatch = new CountDownLatch(EXECUTE_COUNT);
+
+    @Test
+    void initialize() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < EXECUTE_COUNT; i++) {
+            executorService.submit(() -> {
+                insert();
+                countDownLatch.countDown();
+                System.out.println("countDownLatch.getCount() = " + countDownLatch.getCount());
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+    void insert() {
+        transactionTemplate.executeWithoutResult(status -> {
+            for (int i = 0; i < BULK_INSERT_COUNT; i++) {
+                Article article = Article.create(
+                        snowflake.nextId(),
+                        "title" + i,
+                        "content" + i,
+                        1L,
+                        1L
+                );
+                entityManager.persist(article);
+            }
+        });
+    }
+}
