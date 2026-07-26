5fb40b1451520159fc36112fcdd981de0c8284de
jisulee-shsf <jisulee.shsf@gmail.com>
2026-07-26 22:43:09 +0900

    feat: 좋아요 수 구현 - 동시성 제어

--- service/like/src/main/java/board/like/service/ArticleLikeService.java
+++ service/like/src/main/java/board/like/service/ArticleLikeService.java
@@ -2,6 +2,8 @@ package board.like.service;
 
 import board.common.snowflake.Snowflake;
 import board.like.entity.ArticleLike;
+import board.like.entity.ArticleLikeCount;
+import board.like.repository.ArticleLikeCountRepository;
 import board.like.repository.ArticleLikeRepository;
 import board.like.service.response.ArticleLikeResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ public class ArticleLikeService {
 
     private final Snowflake snowflake = new Snowflake();
     private final ArticleLikeRepository articleLikeRepository;
+    private final ArticleLikeCountRepository articleLikeCountRepository;
 
     public ArticleLikeResponse read(Long articleId, Long userId) {
         return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
@@ -22,7 +25,7 @@ public class ArticleLikeService {
     }
 
     @Transactional
-    public void like(Long articleId, Long userId) {
+    public void likePessimisticLock1(Long articleId, Long userId) {
         articleLikeRepository.save(
                 ArticleLike.create(
                         snowflake.nextId(),
@@ -30,11 +33,82 @@ public class ArticleLikeService {
                         userId
                 )
         );
+
+        int result = articleLikeCountRepository.increase(articleId);
+        if (result == 0) {
+            articleLikeCountRepository.save(
+                    ArticleLikeCount.init(articleId, 1L)
+            );
+        }
     }
 
     @Transactional
-    public void unlike(Long articleId, Long userId) {
+    public void unlikePessimisticLock1(Long articleId, Long userId) {
         articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
-                .ifPresent(articleLikeRepository::delete);
+                .ifPresent(articleLike -> {
+                            articleLikeRepository.delete(articleLike);
+                            articleLikeCountRepository.decrease(articleId);
+                        }
+                );
+    }
+
+    @Transactional
+    public void likePessimisticLock2(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId)
+                .orElseGet(() -> ArticleLikeCount.init(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+    @Transactional
+    public void unlikePessimisticLock2(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                            articleLikeRepository.delete(articleLike);
+                            ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId).orElseThrow();
+                            articleLikeCount.decrease();
+                        }
+                );
+    }
+
+    @Transactional
+    public void likeOptimisticLock(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findById(articleId)
+                .orElseGet(() -> ArticleLikeCount.init(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+    @Transactional
+    public void unlikeOptimisticLock(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                            articleLikeRepository.delete(articleLike);
+                            ArticleLikeCount articleLikeCount = articleLikeCountRepository.findById(articleId).orElseThrow();
+                            articleLikeCount.decrease();
+                        }
+                );
+    }
+
+    public Long count(Long articleId) {
+        return articleLikeCountRepository.findById(articleId)
+                .map(ArticleLikeCount::getLikeCount)
+                .orElse(0L);
     }
 }
--- service/like/src/test/java/board/like/api/LikeApiTest.java
+++ service/like/src/test/java/board/like/api/LikeApiTest.java
@@ -4,6 +4,10 @@ import board.like.service.response.ArticleLikeResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestClient;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class LikeApiTest {
 
     RestClient restClient = RestClient.create("http://localhost:9002");
@@ -45,5 +49,51 @@ public class LikeApiTest {
                 .uri("/v1/article-likes/articles/{articleId}/users/{userId}", articleId, userId)
                 .retrieve()
                 .body(ArticleLikeResponse.class);
+    }
+
+    @Test
+    void likePerformanceTest() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        try {
+            likePerformanceTest(executorService, 1L, "pessimistic-lock-1");
+            likePerformanceTest(executorService, 2L, "pessimistic-lock-2");
+            likePerformanceTest(executorService, 3L, "optimistic-lock");
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    void likePerformanceTest(ExecutorService executorService, Long articleId, String lockType) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3000);
+
+        System.out.println("lockType = " + lockType + " start");
+        likeWithLockType(articleId, 1L, lockType);
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 3000; i++) {
+            long userid = i + 2;
+            executorService.submit(() -> {
+                likeWithLockType(articleId, userid, lockType);
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+
+        long end = System.currentTimeMillis();
+        System.out.println("lockType = " + lockType + " / time = " + (end - start) + "ms");
+
+        Long count = restClient.get()
+                .uri("/v1/article-likes/articles/{articleId}", articleId)
+                .retrieve()
+                .body(Long.class);
+
+        System.out.println("count = " + count);
+    }
+
+    void likeWithLockType(Long articleId, Long userId, String lockType) {
+        restClient.post()
+                .uri("/v1/article-likes/articles/{articleId}/users/{userId}/" + lockType, articleId, userId)
+                .retrieve();
     }
 }
